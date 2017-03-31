7b075975824a04a109732c9544b7a137cf670b40
CYRO4S <toonsoftinc@gmail.com>
2017-03-31 11:55:42 -0500

    Update uls_install.sh

--- uls_install.sh
+++ uls_install.sh
@@ -1,16 +1,22 @@
-#!/bin/bash
+#!/bin/bash 
 
 echo "Installing required programs..."
+apt-get -y install wget python3 virt-what dos2unix
+yum -y install wget python3 virt-what dos2unix
 
-apt-get -y install wget python3 virt-what
-yum -y install wget python3 virt-what
+echo "Downloading ULS..." 
+wget https://raw.githubusercontent.com/CYRO4S/Universal-Linux-Script/master/uls.py && chmod +x uls.py 
+dos2unix uls.py
 
-echo "Downloading ULS..."
-wget https://raw.githubusercontent.com/CYRO4S/Universal-Linux-Script/master/uls.py && chmod +x uls.py
+echo "Make ULS a true command..."
+mkdir /usr/share/uls
+mv uls.py /usr/share/uls/uls.py
+ln uls.py /usr/bin/uls
 
-echo "Let ULS get your system information..."
-./uls.py --getinfo
+echo "Let ULS get your system information..." 
+uls --getinfo 
 
-clear
-echo "All Done."
+clear 
+
+echo "All Done." 
 echo "Now use './uls.py [Path-to-ULS-script]' to run a ULS script."
