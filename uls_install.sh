85746b8ab1364985eeee31d07dabaf19b224b1e9
CYRO4S <toonsoftinc@gmail.com>
2017-04-01 08:13:59 +0800

    ULS is now a Linux command after setup!

--- uls_install.sh
+++ uls_install.sh
@@ -1,22 +1,26 @@
-#!/bin/bash 
+#!/bin/bash
 
-echo "Installing required programs..."
-apt-get -y install wget python3 virt-what dos2unix
-yum -y install wget python3 virt-what dos2unix
+echo "Updating source..."
+apt-get update
+yum update
 
-echo "Downloading ULS..." 
-wget https://raw.githubusercontent.com/CYRO4S/Universal-Linux-Script/master/uls.py && chmod +x uls.py 
-dos2unix uls.py
+echo "Installing required packages..."
+apt-get -y install wget python3 virt-what
+yum -y install wget python3 virt-what
 
-echo "Make ULS a true command..."
+echo "Downloading ULS..."
+wget https://raw.githubusercontent.com/CYRO4S/Universal-Linux-Script/master/uls.py && chmod +x uls.py
+
+echo "Let's make ULS a true command..."
 mkdir /usr/share/uls
 mv uls.py /usr/share/uls/uls.py
 ln /usr/share/uls/uls.py /usr/bin/uls
 
-echo "Let ULS get your system information..." 
-uls --getinfo 
+clear
 
-clear 
+echo "Let ULS get your system information..."
+uls --getinfo
 
-echo "All Done." 
+echo "All done."
 echo "Now use 'uls [Path-to-ULS-script]' to run a ULS script."
+
